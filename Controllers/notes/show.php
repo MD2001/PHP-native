7c0916ee1020316cea16b467ce86b8b155f61d48
Mohamed Diaa <mdiea0581@gmail.com>
2025-10-16 15:32:48 +0300

    .

--- Controllers/notes/show.php
+++ Controllers/notes/show.php
@@ -6,13 +6,10 @@ $config = require(base_path('config.php'));
 
 $connection = new Database($config['database'], 'root');
 
-
-
 $data = $connection->quiery("select * from notes where id = :id", ["id" => $_GET['id'] ?? $_POST['id']])->findOrFail();
 
 Authorize($data["user_id"] === 1);
 
-// dd($data);
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $connection->quiery("DELETE from notes where id = :id", ["id" => $_POST["id"]]);
 
