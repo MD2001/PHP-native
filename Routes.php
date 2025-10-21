b13bbffa012591798d24df4e44c6e861e2337f36
Mohamed Diaa <mdiea0581@gmail.com>
2025-10-21 15:54:48 +0300

    reorgniza Routes

--- Routes.php
+++ Routes.php
@@ -1,21 +1,18 @@
  <?php
 
 
-  $router->get('/', "Controllers/home.php");
-  $router->get('/features', "Controllers/features.php");
-  $router->get('/pricing', "Controllers/pricing.php");
-  $router->get('/notes', "Controllers/notes/index.php");
-  $router->get('/note', "Controllers/notes/show.php");
-  $router->get('/create', "Controllers/notes/create.php");
-  $router->get('/note/edit', "Controllers/notes/edit.php");
+    $router->get('/', "Controllers/home.php");
+    $router->get('/features', "Controllers/features.php");
+    $router->get('/pricing', "Controllers/pricing.php");
+    $router->get('/notes', "Controllers/notes/index.php");
+    $router->get('/note', "Controllers/notes/show.php");
+    $router->get('/create', "Controllers/notes/create.php");
+    $router->get('/note/edit', "Controllers/notes/edit.php");
+    $router->get('/register', "Controllers/registers/index.php");
 
+    $router->patch('/note/edit', "Controllers/notes/upDate.php");
 
-  $router->patch('/note/edit', "Controllers/notes/upDate.php");
+    $router->post('/create', "Controllers/notes/addRecored.php");
+    $router->post('/register', "Controllers/registers/login.php");
 
-  $router->post('/create', "Controllers/notes/addRecored.php");
-
-  $router->delete('/note', "Controllers/notes/destroy.php");
-
-
-  $router->get('/register', "Controllers/registers/index.php");
-  $router->post('/register', "Controllers/registers/login.php");
+    $router->delete('/note', "Controllers/notes/destroy.php");
