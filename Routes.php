1099151656e61991a1b127fb15e14308138361bc
Mohamed Diaa <mdiea0581@gmail.com>
2025-10-25 08:19:36 +0300

    .

--- Routes.php
+++ Routes.php
@@ -1,18 +1,18 @@
  <?php
 
 
-   $router->get('/', "Controllers/home.php");
-   $router->get('/features', "Controllers/features.php");
-   $router->get('/pricing', "Controllers/pricing.php");
-   $router->get('/notes', "Controllers/notes/index.php");
-   $router->get('/note', "Controllers/notes/show.php");
-   $router->get('/create', "Controllers/notes/create.php");
-   $router->get('/note/edit', "Controllers/notes/edit.php");
-   $router->get('/register', "Controllers/registers/index.php")->only('guest');
+  $router->get('/', "Controllers/home.php");
+  $router->get('/features', "Controllers/features.php");
+  $router->get('/pricing', "Controllers/pricing.php");
+  $router->get('/notes', "Controllers/notes/index.php")->only('auth');
+  $router->get('/note', "Controllers/notes/show.php")->only('auth');
+  $router->get('/create', "Controllers/notes/create.php")->only('auth');
+  $router->get('/note/edit', "Controllers/notes/edit.php")->only('auth');
+  $router->get('/register', "Controllers/registers/index.php")->only('guest');
 
-   $router->patch('/note/edit', "Controllers/notes/upDate.php");
+  $router->patch('/note/edit', "Controllers/notes/upDate.php")->only('auth');
 
-   $router->post('/create', "Controllers/notes/addRecored.php");
-   $router->post('/register', "Controllers/registers/login.php");
+  $router->post('/create', "Controllers/notes/addRecored.php")->only('auth');
+  $router->post('/register', "Controllers/registers/login.php");
 
-   $router->delete('/note', "Controllers/notes/destroy.php");
+  $router->delete('/note', "Controllers/notes/destroy.php");
