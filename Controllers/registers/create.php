7ca8fb7e989d07a4ed74454757d39cd93ef0bfb4
Mohamed Diaa <mdiea0581@gmail.com>
2025-10-29 14:01:38 +0300

    login_register_forms

--- Controllers/registers/create.php
+++ Controllers/registers/create.php
@@ -15,36 +15,25 @@ if (!Verification::string($_POST["password"], 3, 50)) {
 
 
 $db = App::resolve(Database::class);
-$email = $db->quiery("select * from users where email=:email", [
+$email = $db->quiery("select email from users where email=:email", [
     "email" => $_POST["email"],
-])->find();
-$result = $db->quiery("select * from users where email=:email AND password = :password ", [
-    "email" => $_POST["email"],
-    "password" => $_POST["password"],
 ])->find();
 
-if ($result) {
+if ($email) {
+
+    $error["password"] = "this email is used";
+} else {
+    $db->quiery("INSERT INTO users (email, password) VALUES (:email, :password)", [
+        "email" => $_POST["email"],
+        "password" => password_hash($_POST["password"], PASSWORD_DEFAULT),
+    ]);
     $_SESSION["login"] = true;
     $_SESSION["userid"] = $result["id"];
     $_SESSION["name"] = $result["name"];
     header("location:/");
     exit();
-} else {
-    if ($email) {
-
-        $error["password"] = "this password not correct";
-    } else {
-        $db->quiery("INSERT INTO users (email, password) VALUES (:email, :password)", [
-            "email" => $_POST["email"],
-            "password" => password_hash($_POST["password"], PASSWORD_DEFAULT),
-        ]);
-        $_SESSION["login"] = true;
-        $_SESSION["userid"] = $result["id"];
-        $_SESSION["name"] = $result["name"];
-        header("location:/");
-        exit();
-    }
 }
+
 if (!empty($error)) {
     view("registers/index.view.php", ["error" => $error, "email" => $_POST["email"]]);
     exit();
--- Controllers/session/create.php
+++ Controllers/session/create.php
@@ -2,4 +2,45 @@
 
 //you get from post request the email and pssword
 
-//  her we create session and mange the login form 
+use Core\App;
+use Core\Database;
+use Core\Verification;
+
+
+$error = [];
+if (!Verification::email($_POST["email"])) {
+    $error["email"] = "this email is not correct";
+}
+if (!Verification::string($_POST["password"], 3, 50)) {
+    $error["password"] = "this password is not correct";
+}
+
+
+$db = App::resolve(Database::class);
+$email = $db->quiery("select * from users where email=:email", [
+    "email" => $_POST["email"],
+])->find();
+
+
+
+if ($email && (password_verify($_POST["password"],$email["password"]))) {
+    $_SESSION["login"] = true;
+    $_SESSION["userid"] = $result["id"];
+    $_SESSION["name"] = $result["name"];
+    header("location:/");
+    exit();
+} else {
+    if ($email) {
+        $error["password"] = "this password not correct";
+    } else {
+        $error["email"] = "this email not found";
+    }
+}
+
+if (!empty($error)) {
+    view("session/index.view.php", [
+        "error" => $error,
+        "email" => $_POST["email"],
+    ]);
+    exit();
+}
